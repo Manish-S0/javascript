5dcbdfe158df6766de4ac8938e9ff9d05efd3cde
Manish <106476756+Manish-S0@users.noreply.github.com>
2024-02-19 09:51:29 +0000

    conversion

---
+++ 01_basics/3_conversionOperation.js
@@ -0,0 +1,59 @@
+//***********Number********** */
+// let score=33;
+
+// valueInString=String(score);
+// console.log(valueInString);
+// console.log(typeof valueInString);
+
+// valueInBoolean=Boolean(score);
+// console.log(valueInBoolean);
+// console.log(typeof valueInBoolean);
+
+/*******************Null************/
+
+// let temp=null;
+
+// valueInNumber=Number(temp);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// valueInString=String(temp);
+// console.log(valueInString);
+// console.log(typeof valueInString)
+
+// valueInString=Boolean(temp);
+// console.log(valueInString);
+// console.log(typeof valueInString);
+
+/*************** String *********/
+// let card='3K';
+
+// valueInNumber=Number(card);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+/************* Boolean ************/
+
+// let booleanValue=true;
+
+// valueInNumber=Number(booleanValue);
+// valueInString=String(booleanValue);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInString);
+// console.log(typeof valueInString);
+
+/**********undefined **********/
+// let temp;
+
+
+// console.log(Number(temp));
+// console.log(typeof(Number(temp)));
+
+// console.log(String(temp));
+// console.log(typeof(String(temp)));
+
+// console.log(Boolean(temp));
+// console.log(typeof (Boolean(temp)));
+
