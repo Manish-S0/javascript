07447fb1aaffa77dae7ae46715748a60d6b37c3f
Manish <106476756+Manish-S0@users.noreply.github.com>
2024-02-19 09:14:39 +0000

    variables

---
+++ 01_basics/1_variables.js
@@ -0,0 +1,13 @@
+const accId=123456
+let accEmail='xyz@gmail.com'
+var accPassword='14568'
+accCity='Rome'
+let accCountry;
+
+// accId=23 //not allowed to change const variable
+accEmail='xzy@gmail.com'
+accPassword='4568'
+accCity='janakpur'
+
+console.log(accId);
+console.table([accId,accEmail,accPassword,accCity,accCountry]);
