6839a3a9fff5524ff2d3ebcd61aedbcd9d66392c
Manish <106476756+Manish-S0@users.noreply.github.com>
2024-02-19 09:23:48 +0000

    dataType

---
+++ 01_basics/2_dataTypes.js
@@ -0,0 +1,19 @@
+/*primitive dataType
+
+1.number  => 2 to power 53
+2.string
+3.boolean
+4.null
+5.undefined
+6.bigint
+7.symbol => unique
+
+
+*/
+
+
+let age=24
+
+console.log(typeof age);
+console.log(typeof null)
+console.log(typeof undefined)
