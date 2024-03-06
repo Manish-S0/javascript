f20fd4517a258b3d4345656c768e41242bdafd65
Manish <106476756+Manish-S0@users.noreply.github.com>
2024-03-06 06:20:32 +0000

    guessing a number

---
+++ projects/test/test.js
@@ -0,0 +1,7 @@
+const hi=document.querySelector('.h');
+const button = document.createElement('button');
+button.type='button';
+button.className='newgame';
+button.textContent= 'Start new Game';
+
+hi.appendChild(button);
